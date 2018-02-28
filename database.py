87476261055f493216fe0052f6b2e1e056de68e0
yunusagtas <agtas.yunus@gmail.com>
2018-02-28 14:02:14 +0300

    v1.1

--- database.py
+++ database.py
@@ -12,7 +12,7 @@ class Database:
         self.collection = self.db['adresler']
         self.query_parameters = {}
 
-    #returns new _id for db
+    # returns new _id for db
     def get_next_sequence(self, collection, name):
         return collection.find_and_modify(
             query={'_id': name},
@@ -21,7 +21,7 @@ class Database:
             new=True
         ).get('seq')
 
-    #insert new data
+    # insert new data
     def insert(self, name, surname, mail, phone):
         post_data = {
             '_id': self.get_next_sequence(self.db["idCounter"], "userid"),
@@ -33,24 +33,12 @@ class Database:
         return self.collection.insert_one(post_data).inserted_id
 
     # adds parameters to query_parameters
-    def add_id_query(self, id):
-        self.query_parameters["_id"] = id
+    def add_query_parameter(self, param_name, param_val):
+        self.query_parameters[param_name] = param_val
 
-    def add_name_query(self, name):
-        self.query_parameters["name"] = name
-
-    def add_surname_query(self, surname):
-        self.query_parameters["surname"] = surname
-
-    def add_mail_query(self, mail):
-        self.query_parameters["mail"] = mail
-
-    def add_phone_query(self, phone):
-        self.query_parameters["phone"] = phone
-
-    #mongodb query
+    # mongodb query
     def find(self):
-        query_parameters = self.query_parameters
+        temp_query_parameters = self.query_parameters
         self.query_parameters = {}
-        return self.collection.find(query_parameters)
+        return self.collection.find(temp_query_parameters)
 
--- main.py
+++ main.py
@@ -3,40 +3,55 @@ from flask import Flask, request
 from database import Database
 from bson.json_util import dumps
 import json
+import re
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def on_get():
+    return ""
+
 @app.route('/', methods=['POST'])
-def main():
+def on_post():
+
+    required_insert_parameters = ("name", "surname", "mail", "phone")
+    query_parameters = ("_id", "name", "surname", "mail", "phone")
 
     if request.method == 'POST':
-
-        #insert json data ("name", "surname", "mail", "phone")
         if "insert" in request.form:
 
             json_data = request.form['insert']
             try:
                 data = json.loads(json_data)
             except Exception as e:
-                print (e)
                 return e.message
 
-            if set(("name", "surname", "mail", "phone")) <= set(data):
+            if set(required_insert_parameters) <= set(data):
+                for insert_parameter in data:
+                    if not (insert_parameter in required_insert_parameters):
+                        return "Invalid insert parameter:%s" % insert_parameter
+                    if data[insert_parameter] == "":
+                        return "Empty parameter:%s" % insert_parameter
+                    if insert_parameter == "mail" and not re.match(r"[^@]+@[^@]+\.[^@]+", data["mail"]):
+                        return "Email is not valid"
+
                 inserted_id = adress_database.insert(data["name"], data["surname"], data["mail"], data["phone"])
-                return str(inserted_id) #returns inserted id
+                return str(inserted_id)
             else:
-                return "Missing parameters"
-        else: #query the parameters
-            if "id" in request.form:
-                adress_database.add_id_query(int(request.form['id']))
-            if "name" in request.form:
-                adress_database.add_name_query(request.form['name'])
-            if "surname" in request.form:
-                adress_database.add_surname_query(request.form['surname'])
-            if "mail" in request.form:
-                adress_database.add_mail_query(request.form['mail'])
-            if "phone" in request.form:
-                adress_database.add_phone_query(request.form['phone'])
+                return "Missing insert parameters"
+        else:
+            for posted_parameter in request.form:
+                value = request.form[posted_parameter]
+
+                if not (posted_parameter in query_parameters):
+                    return "Invalid parameter:%s" % posted_parameter
+                if value == "":
+                    return "Empty parameter:%s" % posted_parameter
+
+                if posted_parameter == "_id":
+                    value = int(value)
+
+                adress_database.add_query_parameter(posted_parameter, value)
 
     query_result = list(adress_database.find())
     web_response = dumps(query_result)
